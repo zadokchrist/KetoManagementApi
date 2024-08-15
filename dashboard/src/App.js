bc1742166a456e739d9a87a22a38faf17813351e
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2024-08-15 14:56:34 +0300

    add user routes

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,13 +4,17 @@ import LoginPage from './components/LoginPage';
 import Layout from './components/Layout';
 import ViewPatient from './components/ViewPatient';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Pages/Users/Login';
+import Register from './Pages/Users/Register';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
         <Route path='/layout' element={<Layout/>}/>
       </Routes>
     </BrowserRouter>
